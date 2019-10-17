5adf91f32c167f87361617fe9481d7bb04265f55
Sreehari <sreeharikv112@gmail.com>
2019-10-17 18:57:26 +0530

    Theme changed

--- app/src/androidTest/java/com/sn/quizapp/utils/RecyclerViewMatcher.kt
+++ app/src/androidTest/java/com/sn/quizapp/utils/RecyclerViewMatcher.kt
@@ -1,19 +1,17 @@
 package com.sn.quizapp.utils
 
-import android.content.res.Resources
-import androidx.recyclerview.widget.RecyclerView
 import android.view.View
 import android.widget.TextView
 import androidx.annotation.NonNull
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.matcher.BoundedMatcher
 import com.sn.quizapp.R
 import com.sn.quizapp.ui.gameresult.ScoreListViewHolder
 import org.hamcrest.Description
 import org.hamcrest.Matcher
-import org.hamcrest.TypeSafeMatcher
 
 
-class RecyclerViewMatcher() {
+class RecyclerViewMatcher {
 
 
     fun withHolderTimeView(text: String): Matcher<RecyclerView.ViewHolder> {
